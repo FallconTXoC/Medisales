6373a743fa0da75b6b2cc6f0572a9bb62a927fe2
FallconTXoC <84561183+FallconTXoC@users.noreply.github.com>
2023-04-14 22:24:43 +0200

    front store terminé

--- public/js/store.js
+++ public/js/store.js
@@ -1,3 +1,5 @@
+let selected_products = [];
+
 $('.multi-select').select2({
     theme: 'bootstrap-5',
     width: '14vw',
@@ -9,3 +11,21 @@ $('.multi-select').select2({
 $('.search-input')
     .blur(() => { $('.searchbar').removeClass('focused'); })
     .focus(() => { $('.searchbar').addClass('focused');});
+
+$('.product').click(function(event) {
+    const prodID = $(this).attr("prod-code");
+
+    if($(this).hasClass('active') === true) {
+        $(this).removeClass('active');
+        $(`input[value=${prodID}]`).prop('checked', false);
+        
+        selected_products = selected_products.filter(prod_id => prod_id !== prodID);
+        if (selected_products.length === 0) $('.contract-button').addClass('hidden');
+    } else {
+        $(this).addClass('active');
+        $(`input[value=${prodID}]`).prop('checked', true);
+        $('.contract-button').removeClass('hidden');
+
+        selected_products.push(prodID);
+    }
+})
